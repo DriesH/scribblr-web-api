71739a4b7eee94b5c2b91b8aecf0a3f53937dda7
DriesH <dries.heyninck@gmail.com>
2017-03-21 16:41:54 +0100

    layouts update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,9 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
+    <!-- FACICONS -->
+    <link rel="icon" href="{{ asset('favicos/favicon.ico') }}" type="image/x-icon" />
+
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
