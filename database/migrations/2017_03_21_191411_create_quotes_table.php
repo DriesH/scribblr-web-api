2cb47d0d6729684e82b6216b1c9578c9a89c52d0
jorenvh1 <joren.vh@hotmail.com>
2017-03-21 21:48:58 +0100

    Added migrations, models, removing passport, adding jwtauth + ocnfig

---
+++ database/migrations/2017_03_21_191411_create_quotes_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateQuotesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('quotes', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('short_id', 8)->unique();
+            $table->integer('child_id')->unsigned();
+            $table->string('quote', 300);
+            $table->integer('font_size');
+            $table->string('font');
+            $table->timestamps();
+
+            $table->foreign('child_id')->references('id')->on('children')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('quotes');
+    }
+}
---
+++ database/migrations/2017_03_21_192026_create_book__quotes_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBookQuotesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('book__quotes', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('book_id')->unsigned();
+            $table->integer('quote_id')->unsigned();
+            $table->timestamps();
+
+            $table->foreign('book_id')->references('id')->on('books')->onDelete('cascade');
+            $table->foreign('quote_id')->references('id')->on('quotes')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('book__quotes');
+    }
+}
