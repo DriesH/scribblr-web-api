8bca158d731190bee525e4a4a86be70b3629919f
Dries Heyninck <dries.heyninck@gmail.com>
2017-03-28 20:26:34 +0200

    updated register view and updated the package

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,76 +1,41 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Register</div>
-                <div class="panel-body">
-                    <form class="form-horizontal" role="form" method="POST" action="{{ route('register') }}">
-                        {{ csrf_field() }}
+<div class="register-container theme-1-maincolor">
 
-                        <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
-                            <label for="name" class="col-md-4 control-label">Name</label>
-
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}" required autofocus>
-
-                                @if ($errors->has('name'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('name') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
-
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">Password</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control" name="password" required>
-
-                                @if ($errors->has('password'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <label for="password-confirm" class="col-md-4 control-label">Confirm Password</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Register
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
+    <div class="register-container__logo">
+        <span class="scribblr-logo">Scribblr</span>
     </div>
+
+    <div class="register-container__heading">
+        <h1 id="hello-there" class="big-header-text theme-1-maincolor">Hello there.</h1>
+        <h2 id="new-face" class="sub-header-text theme-1-subcolor">Let's put a name to the new face!</h2>
+    </div>
+
+    <form class="register-container__form" action="{{ route('register') }}" method="POST">
+        {{ csrf_field() }}
+        <div class="register-form__input-field-container">
+            <div class="register-form-input-field">
+                <label for="fullname">Fullname</label>
+                <input id="fullname" type="text" class="form-control" name="fullname" value="{{ old('fullname') }}" required autofocus>
+            </div>
+
+            @if ($errors->has('fullname'))
+                {{ $errors->first('fullname') }}
+            @endif
+
+            <div class="register-form-input-field">
+                <label for="email">Email</label>
+                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
+            </div>
+
+            @if ($errors->has('email'))
+                {{ $errors->first('email') }}
+            @endif
+
+            <button class="btn btn-submit theme-1-subcolor theme-1-maincolor-text" type="submit">Register</button>
+        </div>
+    </form>
+
 </div>
 @endsection
