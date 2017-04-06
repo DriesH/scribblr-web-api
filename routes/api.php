bd55c5a9dda56e74926a8fd370f996ec14bee2e9
jorenvh1 <joren.vh@hotmail.com>
2017-04-06 20:51:47 +0200

    returning user

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
+use stdClass;
 
 /*
 |--------------------------------------------------------------------------
@@ -34,7 +35,22 @@ Route::group(['middleware' => 'auth:api'], function () {
     });
 
     Route::get('/user', function (Request $request) {
-        return response()->json(array($request->user()));
+        $user_resp = new stdClass();
+        $user_resp->id = $user->id;
+        $user_resp->short_id = $user->short_id;
+        $user_resp->first_name = $user->first_name;
+        $user_resp->last_name = $user->last_name;
+        $user_resp->email = $user->email;
+        $user_resp->street_name = $user->street_name;
+        $user_resp->house_number = $user->house_number;
+        $user_resp->city = $user->city;
+        $user_resp->postal_code = $user->postal_code;
+        $user_resp->country = $user->country;
+        $user_resp->JWTToken = $token;
+
+        return response()->json([
+            'user' => $user_resp
+        ]);
     });
 });
 
