1ccff9fc2bf22f388e471f8bcab5d5f50309458c
jorenvh1 <joren.vh@hotmail.com>
2017-04-08 13:41:50 +0200

    creating usercontroller

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use stdClass;
+class UserController extends Controller
+{
+    public function getUser(Request $request) {
+        $user_resp = new stdClass();
+        $user_resp->id = $user->id;
+        $user_resp->short_id = $user->short_id;
+        $user_resp->first_name = $user->first_name;
+        $user_resp->last_name = $user->last_name;
+        $user_resp->email = $user->email;
+        $user_resp->street_name = $user->street_name;
+        $user_resp->house_number = $user->house_number;
+        $user_resp->city = $user->city;
+        $user_resp->postal_code = $user->postal_code;
+        $user_resp->country = $user->country;
+        $user_resp->JWTToken = $token;
+
+        return response()->json([
+            'user' => $user_resp
+        ]);
+    }
+}
