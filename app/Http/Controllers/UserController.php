c2fb08582d8646c5d736d38681fa8a639069e65a
jorenvh1 <joren.vh@hotmail.com>
2017-04-08 14:27:30 +0200

    added use auth;

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use stdClass;
+use Auth;
+
 class UserController extends Controller
 {
     public function getUser(Request $request) {
