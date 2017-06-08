e1895763f32640525220bbae1038ab9322820bf3
jorenvh1 <joren.vh@hotmail.com>
2017-06-08 22:27:34 +0200

    editing newsseeder

--- database/seeds/NewsSeeder.php
+++ database/seeds/NewsSeeder.php
@@ -15,20 +15,31 @@ class NewsSeeder extends Seeder
     public function run()
     {
         DB::table('news')->insert([
-           'title' => 'Welcome To Scribblr!',
-           'message' => 'Hey there
-           We are happy to welcome you to Scribblr! You can start adding your children in the navigation bar on the left side. After adding them, adding quotes to them is as easy as pie.
-
+           'title' => 'Welcome To Scribblr.',
+           'message' => 'Hey there,
+           We are happy to welcome you to Scribblr. You can start adding your children in the navigation bar on the left side. After adding them, adding quotes to them is as easy as pie.
+           </br>
            Please let us know what you think about Scribblr and what we can improve on.
-
+           </br>
            Suggestions for improvements of our services are more than welcome at: <a href="mailto:info@scribblr.be">info@scribblr.be</a>',
            'created_at' => Carbon::now()->format('Y-m-d H:i:s')
        ]);
 
         DB::table('news')->insert([
-           'title' => 'New update! Added more preset images',
-           'message' => 'Hey there
-           We just wanted to let you know that we added some extra preset images for your Scribbles. They look amazing, don\'t they?',
+           'title' => 'Update: new book cover.',
+           'message' => 'Hey there again,
+           </br>
+           We just wanted to let you know that we added some extra book cover designs. They look amazing, don\'t they?',
+           'created_at' => Carbon::now()->format('Y-m-d H:i:s')
+       ]);
+
+        DB::table('news')->insert([
+           'title' => 'Update: added more fonts.',
+           'message' => 'More fonts
+           </br>
+           We know how much our users like to personalize their memories. This is why we added a few more fonts to make your quotes look even more beautiful.
+           </br>
+           Go ahead and try \' em out!',
            'created_at' => Carbon::now()->format('Y-m-d H:i:s')
        ]);
 
